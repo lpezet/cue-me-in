e18567d9ebad715dc64d698036dd061accaa9d1a
Luke Pezet <lpezet@gmail.com>
2020-04-12 13:58:12 -0600

    Json transformation added.

--- src/main/transformMods/mod.ts
+++ src/main/transformMods/mod.ts
@@ -1,9 +1,17 @@
+import { Response } from "../extractMods/httpMod";
+
 class IdentityClass<I> implements Mod<I, I> {
   transform(input: I): Promise<I> {
     return Promise.resolve(input);
   }
 }
 export const Identity = new IdentityClass();
+
+export const Json: Mod<Response, {}> = {
+  transform(input: Response): Promise<{}> {
+    return Promise.resolve(JSON.parse(input.body || "{}"));
+  }
+};
 
 export interface Mod<I, O> {
   transform: (input: I) => Promise<O>;
